4fb5b1ee2ad07ef8c384fd4a4c6da35483883162
Harald Walker <hwalker@tripolis.com>
2013-04-14 17:25:13 +0200

    Sort recipes by name

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,8 @@ class Recipe < ActiveRecord::Base
   self.primary_key = 'recipe_id'
   self.table_name = 'recipe'
 
+  default_scope order('name ASC')
+
   has_many :ingredients, conditions: 'parent_id = -1'
   has_many :directions
   has_many :recipe_notes
