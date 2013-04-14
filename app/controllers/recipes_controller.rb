351ecc6a5fc84c570403c6a15daf9017979acc86
Harald Walker <hwalker@tripolis.com>
2013-04-14 17:40:41 +0200

    Export plain html of a recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,9 +1,21 @@
 class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
+    if params[:download]
+      @export = true
+      send_data(render_to_string(layout: 'export'), filename: export_filename(@recipe), type: 'text/html')
+    else
+      render
+    end
   end
 
   def index
     @recipes = Recipe.all
   end
+
+  private
+  def export_filename(recipe)
+    recipe.name.gsub(/ /,'').underscore + '.html'
+  end
+
 end
